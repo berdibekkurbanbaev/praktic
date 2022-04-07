2d492899519fb509a8bf36977f7ed9e71bd85a53
berdibekkurbanbaev <kurbanbaevberdibek@gmail.com>
2022-04-07 22:08:43 +0300

    change

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -11,17 +11,20 @@ public class Main {
     }catch (Exception e){
             System.out.println(e.getMessage());
         }
+        finally {
+            System.out.println("finish");
+        }
         }
        public static void metod(int a)throws Exception{
         if (a < 1) throw new Exception("get correct number");
 
         int fuct=1;
 
-          for (int i = 1; i < 5; i++) {
-             fuct *= a;
-              System.out.println(fuct);
-        }
+          for (int i = 1; i <= a; i++) {
+             fuct *= i;
 
+        }
+           System.out.println(fuct);
     }
     }
 
